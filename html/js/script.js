dfa6d594e517255405edbbe4adb869dc86dbc8a2
WoodpeckerZeta <nikolai.kucksdorf@live.de>
2020-07-09 18:43:05 +0200

    Flesh out

--- html/js/script.js
+++ html/js/script.js
@@ -1,17 +1,19 @@
-$(".label").on("click", function(){
-    var id = this.id;
-    $.ajax({
-        url: "test.html",
-        context: document.body,
-        complete: function(){
-    
-        }
-    }).done(function() {
-        $( this ).addClass( "done" );
-    });
-})
+$(document).ready(function(){
+    $(".label").on("click", function(){
+        var id = this.id;
+        $.ajax({
+            url: "file:///C:/Users/User/Documents/GitHub/DnD-Anno-1404/html/test.html",
+            context: document.body,
+            complete: function(e){
+                console.log(e);
+            }
+        }).done(function(e) {
+            $( this ).addClass( "done" );
+        });
+    })
+});
 
-$.ajax({
+/* $.ajax({
     url: "test.html",
     method: "POST",
     context: document.body,
@@ -22,4 +24,4 @@ $.ajax({
         console.log(e);
     }
   }).done(function() { $( this ).addClass( "done" );
-  }).fail(function() { });
+  }).fail(function() { }); */
