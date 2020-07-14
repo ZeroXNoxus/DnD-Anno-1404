0bc7a345ee51a4d558a1b68e91b03575fba5fc4b
WoodpeckerZeta <nikolai.kucksdorf@live.de>
2020-07-14 17:27:32 +0200

    Dialog Form fixed elements

--- html/js/script.js
+++ html/js/script.js
@@ -88,18 +88,20 @@ function getTable(tab){
     $('.resp-container').empty();
     $('.popup-content').empty();
     $('.toolbar').removeClass('show');
+    $('.load-dialog').show();
     $.ajax({
         url: "php/qRequest.php?table="+tab+"&where",
         context: document.body,
     }).done(function(r) {
         var response = JSON.parse(r);
         var table = "<table class='table table-striped table-bordered' data-page-length='25' width='min-width:1800px'>";
-        var form = "<div class='row'><div class='col-12'><form class='dialog-form' data-tab='"+tab+"' method='post'><div class='form-group'><h4>Datensatz anlegen für Tabelle '"+tab+"'</h4><div class='row'>";
+        var form = "<div class='row'><div class='col-12'><form class='dialog-form' data-tab='"+tab+"' method='post'><div class='form-group col-12'><h4 class='header'>Datensatz anlegen für Tabelle '"+tab+"'</h4><div class='row'>";
         var i = 0;
         while(i<response.content.length){
+            var obj = response.content[i];
             if(i===0){
                 table += "<thead>";
-                for(x in response.content[i]){
+                for(x in obj){
                     table += "<th>";
                     table += x;
                     table += "</th>";
@@ -108,14 +110,15 @@ function getTable(tab){
                 table += "<tbody>";
             }
             table += "<tr>";
-            for(x in response.content[i]){
+            for(x in obj){
+                var value = response.content[i][x];
                 var is_pri = false;
                 if(response.header[0]){ if(response.header[0].Column_name == x){ is_pri = true; } }
                 table += "<td>";
                 if(is_pri){
-                    table += "<input type='text' class='is_pri' data-tab='"+tab+"' data-name='"+x+"' value='"+response.content[i][x]+"' />";
+                    table += "<input type='text' class='is_pri' data-tab='"+tab+"' data-name='"+x+"' data-title='"+x+": "+value+"' title='"+x+": "+value+"' value='"+value+"' />";
                 } else{ 
-                    table += "<input type='text' data-tab='"+tab+"' data-name='"+x+"' value='"+response.content[i][x]+"' />";
+                    table += "<input type='text' data-tab='"+tab+"' data-name='"+x+"' data-title='"+x+": "+value+"' title='"+x+": "+value+"' value='"+value+"' />";
                 }
                 if(i == 0){
                     if(is_pri){
@@ -129,10 +132,11 @@ function getTable(tab){
             table += "</tr>";
             i++;
         }
-        form += "</div><div class='pt-3'><button type='submit' class='btn btn-primary'>Submit</button></div></div></form></div></div>";
+        form += "</div><div class='footer'><button type='submit' class='btn btn-primary'>Submit</button></div></div></form></div></div>";
         table += "</tbody></table>";
         $('.resp-container').append(table);
         $('.popup-content').append(form);
+        $('.load-dialog').hide();
         $('.toolbar').addClass('show');
         $('.resp-container table').DataTable({ paging: true });
         bindEventAll();
