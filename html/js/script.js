c6d940bd7226c7ac6c99ec9ef5460d031644fa30
WoodpeckerZeta <nikolai.kucksdorf@live.de>
2020-07-15 16:27:20 +0200

    Add key-Events
    - Added on-Escape-Event; Triggers popup close + mass-edit switch
    - Added on-Enter-Event; Triggers submit for visible button

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    document.addEventListener('keydown', checkKey);
     $('.nav-close-btn>.btn').on('click', function(){
         if(!$('#navbar').hasClass('show')){
             $(this).addClass('btn-danger').removeClass('btn-primary');
@@ -36,12 +37,14 @@ function bindEventAll(){
         var i = 0;
         var param = "";
         var seperator = "";
+        $('.load-dialog').show();
         while(i<input_elements.length){
-            param += (seperator+input_elements[i].id+"="+input_elements[i].value);
+            var str = input_elements[i].id;
+            str = str.split(".")[0];
+            param += (seperator+str+"="+input_elements[i].value);
             seperator = "&";
             i++;
         }
-        $('.load-dialog').show();
         if($(this).hasClass('insert-form')){
             document.cookie = "qType=INSERT; path=/";
         } else if($(this).hasClass('update-form')){
@@ -90,7 +93,7 @@ function bindEventAll(){
     });
     $('.insert-btn').off('click');
     $('.insert-btn').on('click', function(){
-        $("#popup-insert").show();
+        $("#popup-insert").show().focus();
     });
     $('.popup').off('click');
     $('.popup').on('click', function(){
@@ -102,20 +105,38 @@ function bindEventAll(){
     });
     $('.edit-btn').off('click');
     $('.edit-btn').on('click', function(){
-        var selected = $('.dataTable tr.selected input');
-        var i = 0;
-        while(i<selected.length){ if($(selected[i]).hasClass('is_pri')){ $('span.titleId').text(selected[i].value) } $('.update-form input#'+selected[i].dataset.name).val(selected[i].value); i++; }
         if($('.mass-edit-btn.btn-success').length != 0){
             alert("Bitte schalten Sie zuerst den Massen-Bearbeitungs-Modus aus!");
             return;
         }
-        $('#popup-update').show();
+        var selected = $('.dataTable tr.selected input');
+        var i = 0;
+        while(i<selected.length){ 
+            if($(selected[i]).hasClass('is_pri')){ 
+                $('span.titleId').text(selected[i].value)
+            } 
+            $('.update-form input#'+selected[i].dataset.name+"\\.2").val(selected[i].value); 
+            i++; 
+        }
+        $('#popup-update').show().focus();
     });
     $('.paginate_button').on('click', function(){
         bindRowEvent();
     });
     bindRowEvent();
 };
+
+function checkKey(e){
+    if(e.keyCode == 27){
+        $('.popup').hide();
+        if($('.mass-edit-btn').hasClass('btn-success')){
+            $('.mass-edit-btn').removeClass('btn-success').addClass('btn-primary');
+            $('.tab-val').toggleClass('hidden');
+        }
+    } else if(e.keyCode == 13){
+        $('button[type=submit]:visible').click();
+    }
+}
 
 function bindRowEvent(){
     $('.edit-btn').addClass('disabled');
@@ -152,9 +173,9 @@ function getTable(tab){
         context: document.body,
     }).done(function(r) {
         var response = JSON.parse(r);
-        var table = "<table class='table table-striped table-bordered' data-page-length='25' width='min-width:1800px'>";
+        var table = "<table class='table table-striped table-bordered' data-page-length='25' width=''>";
         var form = "<div class='row'><div class='col-12'><form class='popup-form' data-tab='"+tab+"' method='post'><div class='form-group col-12'><h4 class='header'>Datensatz anlegen für Tabelle '"+tab+"'</h4><div class='row'>";
-        var alt_text = tab+" mit ID '<span class='titleId'></span>' bearbeiten"
+        var alt_text = tab+" mit ID '<span class='titleId'></span>' bearbeiten";
         var i = 0;
         while(i<response.content.length){
             var obj = response.content[i];
@@ -200,6 +221,12 @@ function getTable(tab){
         $('.popup-update').empty();
         $('.popup-update').append(form);
         $('.popup-update form').addClass('update-form');
+        var i = 0;
+        var selector = $('.update-form input');
+        while(i<selector.length){
+            selector[i].id = selector[i].id+".2";
+            i++;
+        }
         $('.popup-update form h4').html(alt_text);
         $('.load-dialog').hide();
         $('.toolbar').addClass('show');
