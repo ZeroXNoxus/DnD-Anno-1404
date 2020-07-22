0757939e38700ac768d0aa1116b5d9178f52f1c5
MrROFL <68646402+MrROFL@users.noreply.github.com>
2020-07-22 15:38:01 +0200

    added commentary

--- html/js/map.js
+++ html/js/map.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var map = L.map('map', {
         crs: L.CRS.Simple
     });
-
+//Coordinates scheme is Y/X instead of X/Y
     var bounds = [[0,0], [900,1200]];
     var image = L.imageOverlay('img/World.jpg', bounds).addTo(map);
 
